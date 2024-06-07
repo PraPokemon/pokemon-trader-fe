383507e110fb5aebede2f248a88037cde81b0fac
MarkovicMaks <markovic.maks@gmail.com>
2024-06-07 12:31:27 +0200

    rad na add pokemon i login modalima

--- src/Components/AddToInventoryButon.jsx
+++ src/Components/AddToInventoryButon.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
 function addToInventoryButon() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const [sliderValue, setSliderValue] = useState(50);
+  const handleSliderChange = (e) => {
+    setSliderValue(e.target.value); 
+  };
+
   return (
     <>
-      
       <Button className="ButonBasic" variant="primary" onClick={handleShow}>
         Add to inventory
       </Button>
@@ -20,18 +24,27 @@ function addToInventoryButon() {
           <Modal.Title>Add Pokemon</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <Form.Group className="mb-3"F controlId="exampleForm.ControlInput1">
-              <Form.Label>Pokemon mooves</Form.Label>
-              <Form.Control
-                placeholder="Tackle"
+         <Form.Label>Moves</Form.Label>
+          <Form.Select aria-label="Default select example">
+            <option>Open this select menu</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </Form.Select>
+
+          <div className="outer">
+            <div>
+              <Form.Label>Level Slider</Form.Label>
+              <p>LVL: {sliderValue}</p>{" "}
+              <Form.Range
+                value={sliderValue}
+                name="hello"
+                onChange={handleSliderChange}
+                className="custom-slider"
               />
-        </Form.Group>
-        <Form.Group className="mb-3"F controlId="exampleForm.ControlInput1">
-              <Form.Label>Lvl</Form.Label>
-              <Form.Control
-                placeholder="33"
-              />
-        </Form.Group>
+            </div>
+          </div>
+
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
@@ -43,8 +56,6 @@ function addToInventoryButon() {
         </Modal.Footer>
       </Modal>
     </>
-  
-    
   );
 }
 
--- src/Components/LoginModal.jsx
+++ src/Components/LoginModal.jsx
@@ -23,10 +23,19 @@ function loginModal() {
             <Form.Label>Username</Form.Label>
             <Form.Control placeholder="Lord-Helix" />
           </Form.Group>
-          <Form.Group className="mb-3" F controlId="login">
-            <Form.Label>Pasword</Form.Label>
-            <Form.Control placeholder="*******" />
-          </Form.Group>
+          <>
+            <Form.Label htmlFor="inputPassword5">Password</Form.Label>
+            <Form.Control
+              type="password"
+              id="inputPassword5"
+              aria-describedby="passwordHelpBlock"
+            />
+            <Form.Text id="passwordHelpBlock" muted>
+              Your password must be 8-20 characters long, contain letters and
+              numbers, and must not contain spaces, special characters, or
+              emoji.
+            </Form.Text>
+          </>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
