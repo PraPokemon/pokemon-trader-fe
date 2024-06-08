7e4a21c94d68f4b63084936d469b86acb53b2fce
AlenBosanac <abosanac@algebra.hr>
2024-06-08 12:35:16 +0200

    userpokemonid

---
+++ backend/src/main/java/com/example/backend/model/UserPokemon.java
@@ -0,0 +1,49 @@
+package com.example.backend.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "usersPokemon")
+
+public class UserPokemon {
+@Id
+private int userPokemonId;
+private int userId;
+private int pokemonId;
+
+public UserPokemon() {}
+
+public UserPokemon(int userPokemonId, int userId, int pokemonId) {
+    this.userPokemonId = userPokemonId;
+	this.userId = userId;
+    this.pokemonId = pokemonId;
+}
+
+public int getUserPokemonId() {
+    return userPokemonId;
+}
+
+public void setUserPokemonId(int userPokemonId) {
+    this.userPokemonId = userPokemonId;
+}
+
+public int getUserId() {
+    return userId;
+}
+
+public void setUserId(int userId) {
+    this.userId = userId;
+}
+   
+public int getPokemonId() {
+        return pokemonId;
+    }
+
+public void setPokemonId(int pokemonId) {
+    this.pokemonId = pokemonId;
+}
+
+}
+
+
+
