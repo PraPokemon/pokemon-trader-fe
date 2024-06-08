76d0f13d71d6392091cb5ea9297b7b7d9f07a9e7
Lukas M. <lukas.miholiclol@gmail.com>
2024-06-08 12:54:10 +0200

    Merge branch 'MaksPokedexDev' of https://github.com/PraPokemon/pokemon-trader-fe.git into MaksPokedexDev

--- backend/src/main/java/com/example/backend/model/UserPokemon.java
+++ backend/src/main/java/com/example/backend/model/UserPokemon.java
@@ -3,7 +3,7 @@ package com.example.backend.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "usersPokemon")
+@Document(collection = "userPokemon")
 
 public class UserPokemon {
 @Id
