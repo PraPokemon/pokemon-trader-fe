eac66fbbeaa7d5fbdf6bb2db036a646b69dfe345
AriiesCS <pavel.mokracek@gmail.com>
2024-07-02 18:54:35 +0200

    Isuse 2000 sati popravljanja bugova searchdropdowna napokon popravljeno iz 500og puta!!!!!

--- src/Components/PokemonSearchDropdown.jsx
+++ src/Components/PokemonSearchDropdown.jsx
@@ -1,48 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
 import FormControl from 'react-bootstrap/FormControl';
 import InputGroup from 'react-bootstrap/InputGroup';
 
 const PokemonSearchDropdown = ({ options, onSelect }) => {
     const [searchTerm, setSearchTerm] = useState('');
-    const [filteredOptions, setFilteredOptions] = useState(options);
-    const [selectedPokemon, setSelectedPokemon] = useState(null);
+    const [filteredOptions, setFilteredOptions] = useState([]);
+    const [showDropdown, setShowDropdown] = useState(false);
+    const inputRef = useRef(null);
 
     const handleSearchChange = (e) => {
         const searchValue = e.target.value.toLowerCase();
         setSearchTerm(searchValue);
+
         const filtered = options.filter(option =>
             option.toLowerCase().includes(searchValue)
         );
         setFilteredOptions(filtered);
+        setShowDropdown(filtered.length > 0 && searchValue.length > 0);
     };
 
     const handleItemClick = (option) => {
-        setSelectedPokemon(option);
+        setSearchTerm(option); //updejtaj search sa selektanon opcijon
         onSelect(option);
-        setSearchTerm('');
+        setShowDropdown(false); //zatvori dropdown nakon selektiranja
+    };
+
+    const handleInputFocus = () => {
+        setShowDropdown(filteredOptions.length > 0 && searchTerm.length > 0);
+    };
+
+    const handleInputBlur = () => {
+        setTimeout(() => {
+            setShowDropdown(false);
+        }, 200); // delay skrivanja dropdowna jer neznan ni ja vise
     };
 
     return (
-        <DropdownButton id="dropdown-item-button" title={selectedPokemon || 'Select Pokemon'} className="mt-3">
+        <Dropdown show={showDropdown} drop="start" onClose={() => setShowDropdown(false)}>
             <InputGroup>
                 <FormControl
-                    placeholder="Search..."
+                    ref={inputRef}
+                    placeholder="Search Pokemon..."
                     value={searchTerm}
-                    onChange={handleSearchChange}/>
-            </InputGroup>
-            <Dropdown.Divider />
-            {filteredOptions.map((option, index) => (
-                <Dropdown.Item
-                    key={index}
-                    as="button"
-                    onClick={() => handleItemClick(option)}
+                    onChange={handleSearchChange}
+                    onFocus={handleInputFocus}
+                    onBlur={handleInputBlur}
+                />
+                <Dropdown.Menu
+                    show={showDropdown}
+                    align="start"
+                    style={{ minWidth: '100%', marginTop: '38px' }}
                 >
-                    {option}
-                </Dropdown.Item>
-            ))}
-        </DropdownButton>
+                    {filteredOptions.map((option, index) => (
+                        <Dropdown.Item
+                            key={index}
+                            as="button"
+                            onClick={() => handleItemClick(option)}
+                        >
+                            {option}
+                        </Dropdown.Item>
+                    ))}
+                </Dropdown.Menu>
+            </InputGroup>
+        </Dropdown>
     );
 };
+
 export default PokemonSearchDropdown;
