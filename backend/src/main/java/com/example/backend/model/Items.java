e8170db2e301beac8dbff97fca656712e12dbae7
Lukas M. <lukas.miholiclol@gmail.com>
2024-07-02 14:46:22 +0200

    mini commit

--- backend/src/main/java/com/example/backend/model/Items.java
+++ backend/src/main/java/com/example/backend/model/Items.java
@@ -3,14 +3,12 @@ package com.example.backend.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import com.example.backend.model.Pokemon.Move;
-import com.example.backend.model.Pokemon.Type;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
-import java.util.List;
+
 
 @Document(collection = "items")
 @Data
